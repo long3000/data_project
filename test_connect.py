a2eb61b06ac40b10ab80485d35f1d425da85cae1
Nate Ng <nateng@Nate-MBPro-19.lan>
2024-02-19 18:57:17 -0800

    Re-add cred locally

--- test_connect.py
+++ test_connect.py
@@ -1,21 +1,28 @@
 import psycopg2 as pg 
 from psycopg2 import Error as pg_error
 import pandas as pd
-import os
+import os, json
 
 csv_path = '/CSV'
 csv_file = 'laptop.csv'
 file_path = os.path.join(csv_path, csv_file) 
 
-print(file_path)
+cred_path = '/configs'
+cred_file = 'cred.json'
+cred_path = os.path.join(cred_path, cred_file) 
 
-conn = pg.connect(
-    database = 'crypto_daily',
-    host = '192.168.0.10',
-    user = 'nate.nguyen',
-    password = 'fantasy88',
-    port = 5432
-    )
+print(cred_path)
+cred_parsed = json.loads(cred_path)
+
+print( cred_parsed['username'])
+
+# conn = pg.connect(
+#     database = 'crypto_daily',
+#     host = '192.168.0.10',
+#     user = cred_parsed['username'],
+#     password = cred_parsed['password'],
+#     port = 5432
+#     )
 
 
 # cursor = conn.cursor()
